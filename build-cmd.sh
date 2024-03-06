d49c2183dacc2aa9355131e5198299907aa038d6
RunitaiLinden <davep@lindenlab.com>
2024-03-06 10:54:04 -0600

    Move .dlls to lib/release

--- build-cmd.sh
+++ build-cmd.sh
@@ -51,8 +51,7 @@ windows*)
         cmake $(cygpath -w "$srcdir") -DCMAKE_INSTALL_PREFIX=$(cygpath -w "$top/release")
         cmake --build . --target install --config Release -j
         cp -v ../release/lib/*.lib "$stage/lib/release/"
-        mkdir -p "$stage/bin"
-        cp -v ../release/bin/*.dll "$stage/bin"
+        cp -v ../release/bin/*.dll "$stage/lib/release"
 ;;
 darwin*|linux64*)
         
